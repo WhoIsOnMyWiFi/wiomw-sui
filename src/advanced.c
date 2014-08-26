55154422875d621c28e77881a4d7460594ed78fc
Charles Southerland <charlie@stuphlabs.com>
2014-08-26 17:50:32 -0500

    Adding some basic content

--- src/advanced.c
+++ src/advanced.c
@@ -6,6 +6,10 @@ int main()
 	printf("<html><head><title>%s - %s</title></head><body>",
 			"OpenWRT SUI", "Advanced Options");
 
+	printf("<h1>%s</h1>", "Advanced Options");
+
+	printf("<p><i>Coming soon...</i></p>");
+
 	printf("<h4><a href=\"/index.cgi\">%s</a></h4>", "Basic Options");
 
 	printf("</body></html>");
--- src/index.c
+++ src/index.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 
+void print_wiomw_login_form()
+{
+	char* old_username = "";
+
+	printf("<form action=\"/index.cgi\" method=\"POST\">");
+	printf("<fieldset><legend>%s</legend>", "Who Is On My WiFi Login");
+	printf("<table><tr>");
+	printf("<td>%s</td><td><input type=\"text\" name=\"username\">",
+			"Username");
+	printf("%s</input></td></tr><tr>", old_username);
+	printf("<td>%s</td><td><input type=\"password\" name=\"password\" />",
+			"Password");
+	printf("</td></tr></table><input type=\"submit\" value=\"%s\" />",
+			"Login");
+	printf("</fieldset></form>");
+}
+
 int main()
 {
 	printf("Content-type: text/html\n\n<!DOCTYPE html>");
 	printf("<html><head><title>%s</title></head><body>", "OpenWRT SUI");
+
+	printf("<h1>%s</h1>", "Basic Options");
+
+	print_wiomw_login_form();
 
 	printf("<h4><a href=\"/advanced.cgi\">%s</a></h4>", "Advanced Options");
 
