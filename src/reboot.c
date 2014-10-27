a8ccd64f6697b1c6d8defb02a5faf946acb2f8ff
Charles Southerland <charlie@stuphlabs.com>
2014-10-27 16:39:40 -0500

    Switching reboot to return a boolean

--- src/reboot.c
+++ src/reboot.c
@@ -11,11 +11,11 @@ void post_reboot()
 	if (system(REBOOT_COMMAND) == -1) {
 		printf("Status: 500 Internal Server Error\n");
 		printf("Content-type: application/json\n\n");
-		printf("{\"rebooting\":0,\"errors\":[\"Unable to reboot system.\"]}");
+		printf("{\"rebooting\":false,\"errors\":[\"Unable to reboot system.\"]}");
 		return;
 	}
 	printf("Status: 200 OK\n");
 	printf("Content-type: application/json\n\n");
-	printf("{\"rebooting\":1}");
+	printf("{\"rebooting\":true}");
 }
 
